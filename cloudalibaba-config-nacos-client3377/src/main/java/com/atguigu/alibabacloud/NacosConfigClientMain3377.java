19c48c614d50670240e64189d26d63de24333664
ytq2022 <ytq_glb@126.com>
2022-07-03 22:41:21 +0800

    增加nacos作为配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/alibabacloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.atguigu.alibabacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @create by  ytq
+ * @Date 2022-7-3  17:33
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
